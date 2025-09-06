c1a032c7405b50ce21d5972f2e1f3d7aeb409e1e
Arsenty Dafintsev <aaapython24@gmail.com>
2025-09-07 00:20:12 +0300

    Fix typos

--- src/users/tasks.py
+++ src/users/tasks.py
@@ -18,7 +18,7 @@ def send_activation_email(user_email, activation_url):
 def send_password_reset_link_email(user_email, reset_link):
     """Задача Celery для отправки ссылки-токена на сброс пароля."""
 
-    subject = 'Сброс пароля на сайте "Доска объявлений"',
-    message = f'Для сброса пароля перейдите по ссылке: {reset_link}',
+    subject = 'Сброс пароля на сайте "Доска объявлений"'
+    message = f'Для сброса пароля перейдите по ссылке: {reset_link}'
 
     send_mail(subject, message, EMAIL_HOST_USER, [user_email], fail_silently=False)
